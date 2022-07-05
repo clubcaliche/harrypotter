ef92d06c0855b33bedc515a806e04409f01d9826
clubcaliche <cmoreira@misfirm.com>
2022-07-05 19:22:13 -0400

    Initial commit

---
+++ photos.sh
@@ -0,0 +1,47 @@
+rm pictures.html
+cat << EOF >> pictures.html
+<!DOCTYPE html> 
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+p {
+  color: blue;
+  font-family: verdana;
+  font-size: 100%;
+}
+</style>
+</head>
+<body>
+
+<div class="w3-container w3-teal">
+  <h1>Harry Potter Characters</h1>
+</div>
+EOF
+
+curl -s -X 'GET' \
+    'https://hp-api.herokuapp.com/api/characters' \
+    -H 'accept: application/json' | \
+    jq -r '.[] | "\(.image) \(.name)"' | \
+    while read IMAGE NAME
+
+do
+    cat << EOF >> pictures.html
+    <div class=w3-card-4>
+    </div>
+    <div class=w3-third>
+      <div class=w3-card>
+          <img src=$IMAGE style=width:100%>
+          <div class=w3-container>
+              <h4>$NAME</h4>
+          </div>
+      </div>
+    </div>
+EOF
+done
+
+cat << EOF >> pictures.html
+</body> 
+</html>
+EOF
